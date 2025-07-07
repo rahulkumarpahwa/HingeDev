f72a976f8c30e0b27c9bcf4984d1d928d88ea3af
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-07-08 04:25:23 +0530

    Refactor project structure and add user authentication features
    - Updated package.json and package-lock.json to include dotenv and mongoose dependencies.
    - Implemented database connection logic in a new config/database.js file.
    - Enhanced app.js to connect to the database before starting the server.
    - Created user authentication middleware in src/middlewares/auth.js.
    - Defined user schema in src/models/userSchema.js.
    - Added user signup and data retrieval routes in app.js.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,25 +1,65 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Namaste World!");
-});
-
-app.get("/start", (req, res) => {
-  res.send({ apple: "is the best device" });
-});
-
-
-app.use("/a", (req, res) => {
-  res.send("Hello from A server!");
-});
-
-app.use((req, res) => {
-  res.send("Hello from server!");
-});
-
+const { connectDB } = require("./config/database.js");
+const { User } = require("./models/userSchema.js");
 const port = 8000;
+connectDB()
+  .then(() => {
+    console.log("database connected successfully!");
+    app.listen(port, () => {
+      console.log(`server is successfully listening at port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-app.listen(port, () => {
-  console.log(`server is successfully listening at port ${port}`);
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
+  const newUser = new User(req.body);
+  try {
+    await newUser.save();
+    res.send("data added successfully!");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
+
+app.get("/feed", async (req, res) => {
+  try {
+    const data = await User.find({});
+    console.log(data);
+    // res.send("data founded successfully! " + "data is : \n" +  data + data.length);
+    res.json({ success: true, data: data, length: data.length });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+    console.log("User deleted successfully!");
+    res.send("deleted user" + deletedUser);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  // const userId = req.body.userId;
+  // const firstName = req.body.firstName;
+  const {userId, firstName} = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, {firstName : firstName}, { new: true });
+    console.log("User Updated successfully!");
+    // res.json({changed : [{ updatedUser }]});
+    res.json({ updatedUser });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
