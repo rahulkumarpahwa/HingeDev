142f3600ef9049ebfd857588dbe90cd739824992
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-07-24 20:29:00 +0530

    api validation

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -75,3 +75,64 @@ app.patch("/user", async (req, res) => {
 // 2. there are multiple ways to do that.
 // 3. we are not worried about the get api but we are majorly worried about the post api ans patch api which are inserting data in the database. so we need to put checks here.
 // 4. but before that we will put the checks on the schema itself.
+
+app.patch("/user/:userId", async (req, res) => {
+  // API validations
+  const userId = req.params?.userId; // we will take the userId from the params.
+  const data = req.body;
+  try {
+    // const ALLOWED_UPDATES = ["userId", "about", "photoUrl", "skills"]; // userId is must as it is needed to get the data updated. but userId is not allowed to be updated.
+
+    const ALLOWED_UPDATES = ["about", "photoUrl", "skills"];
+    // userId id needed but to secure it from being updated we will take tahat as params.
+    const isUpdateAllowed = Object.keys(data).every(
+      (k) => ALLOWED_UPDATES.includes(k) // k represent the each key.
+    );
+
+    // one check
+    if (!isUpdateAllowed) {
+      throw new Error("Update not Alllowed");
+    }
+
+    // skills length validation
+    if (data?.skills.length > 10) {
+      throw new Error("Skills can't be more than 10.");
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId, // Id to identify the user.
+      data, // data to be updated
+      { new: true, runValidators: true } // options objects
+    );
+
+    console.log("User Updated successfully!");
+    // res.json({changed : [{ updatedUser }]});
+    res.json({ updatedUser });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+// now we will do the API validation ie. check the data coming in the API before making it store in the DB. this ensures that only valid data is inputted in the database and safe it from the attacks by injection in the database.
+// eg : the api tries to update the emailId. but we want that our user will put the email once only as in the most platforms so in such cases we will put the API validations to safe from the such changes to try to change the email. as the Email is sensitive. It act as indentity.
+// eg 2: if we pass the scrap along with post request in the postman, then our API is not sanitizing such request and data is still updated. so in such cases, we will use API validations.
+
+// so we will do the changes in PATCH route and ignore any changes in the email.
+
+// steps :
+// 1. create a array of 'allowed updates' of the keys of the data object.
+//2. for each key (k) of the data (request.body) coming of out the request check with array above using the includes method and it should be in the Allowed_Updates field/array.
+// 3. Also make sure to add the userId in it as it is needed to update the document. but userId is not allowed to be updated. so for that we will take userId as params in the req.url as
+// const userId = req.params?.userId; make sure to put the ? so as to have no error if no value of the userId is passed.
+//4. if any of the field is additional throw error.
+// 5. update the field and return the response that user has been updated.
+// 6. use the try-catch as before.
+
+// we always need to sanitise the data coming from the api. never trust data coming from the user.
+
+// eg 2: the skills arrays must have the length not more than 10. this might be helpful when a hacker sends about the 1Millions skills in the array and DB may break.
+// so we can put the check for that as :
+//skills length validation
+// if (data?.skills.length > 10) {
+//   throw new Error("Skills can't be more than 10.");
+ // }
