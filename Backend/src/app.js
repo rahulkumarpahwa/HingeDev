08213f5443842be007fe61c1a2aed4f3451583b7
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-07-25 07:17:19 +0530

    validation in the signup using the utils/validation.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { connectDB } = require("./config/database.js");
 const { User } = require("./models/userSchema.js");
+const { validateSignUpData } = require("./utils/validation.js");
 const port = 8000;
 connectDB()
   .then(() => {
@@ -17,15 +18,39 @@ connectDB()
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
-  const newUser = new User(req.body);
   try {
+    console.log(req.body);
+    validateSignUpData(req); // validating the data before saving.
+    const newUser = new User(req.body);
     await newUser.save();
     res.send("data added successfully!");
   } catch (error) {
-    res.status(400).send(error.message);
+    res.status(400).send("error : " + error.message);
   }
 });
+
+// ep 2.9
+// Safely storing the passwords in the database and authenticate as well.
+// 1. first thing is store the password not in the string simple but in the hashed format.
+// 2. so password should be hashed ie. encrypted.
+// 3. so we will update our signup API. As we can't trust the data send by the user in API so we will make it more secure and encrypted.
+// steps :
+// 1. validate the data.
+// 2. encrypt the password.
+// 3. store the user data.
+// 4. to validate the data send by the user we will create some helper functions or middlewares which can be used to do that.
+// 5. Steps for creating a helper function for validating the data.
+// A. create utils/helpers folder in the src folder.
+// B. create a validation.js file.
+// C. create a function validateSignUpData as we are validating the signup data given by the user.
+// D. take the agruments req (request) in the method.
+// E. we will extract the req.body and take out the firstName, lastName, emailId and password.
+// F. then we will check :
+// i. check if the firstName and lastName does not exist then throw error.
+// ii. check the email using the validator.isEmail(value) from the validator package otherwise throw error.
+// iii. check the password using the validator.isStrongPassword(value) from the validator package else throw error.
+// export the function using module.exports and then import in the app.js
+// 6. put the method inside the signup route and then pass the req in the function validateSignUpData(req). make sure to have the try-catch block to catch the error occur.
 
 app.get("/feed", async (req, res) => {
   try {
@@ -48,33 +73,6 @@ app.delete("/user", async (req, res) => {
     res.status(400).send(error.message);
   }
 });
-
-app.patch("/user", async (req, res) => {
-  // here we are patching the firstname (updating the firstname)
-  // const userId = req.body.userId;
-  // const firstName = req.body.firstName;
-  const { userId, firstName } = req.body;
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      userId, // Id to identify the user.
-      { firstName: firstName }, // data to be updated
-      { new: true, runValidators: true } // options objects :  1. new will returned the new user. 2.this will check the custom validations for the updated user acording to the schema.
-    );
-    // the custom validations we have added in our schema will work only for the new User object created and not for the existing object and to make that work we will have use the [options] under the Model.findByIdAndUpdate() method.
-    // for that we will pass a custom options in method as '{runValidators : true}' after the userId, data to be updated and then the options.
-
-    console.log("User Updated successfully!");
-    // res.json({changed : [{ updatedUser }]});
-    res.json({ updatedUser });
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
-
-// 1. validations : until now we are adding the garbage in the database but here we will make strict checks on the data entered by user so that only valid data is to be added in the database.
-// 2. there are multiple ways to do that.
-// 3. we are not worried about the get api but we are majorly worried about the post api ans patch api which are inserting data in the database. so we need to put checks here.
-// 4. but before that we will put the checks on the schema itself.
 
 app.patch("/user/:userId", async (req, res) => {
   // API validations
@@ -112,27 +110,3 @@ app.patch("/user/:userId", async (req, res) => {
     res.status(400).send(error.message);
   }
 });
-
-// now we will do the API validation ie. check the data coming in the API before making it store in the DB. this ensures that only valid data is inputted in the database and safe it from the attacks by injection in the database.
-// eg : the api tries to update the emailId. but we want that our user will put the email once only as in the most platforms so in such cases we will put the API validations to safe from the such changes to try to change the email. as the Email is sensitive. It act as indentity.
-// eg 2: if we pass the scrap along with post request in the postman, then our API is not sanitizing such request and data is still updated. so in such cases, we will use API validations.
-
-// so we will do the changes in PATCH route and ignore any changes in the email.
-
-// steps :
-// 1. create a array of 'allowed updates' of the keys of the data object.
-//2. for each key (k) of the data (request.body) coming of out the request check with array above using the includes method and it should be in the Allowed_Updates field/array.
-// 3. Also make sure to add the userId in it as it is needed to update the document. but userId is not allowed to be updated. so for that we will take userId as params in the req.url as
-// const userId = req.params?.userId; make sure to put the ? so as to have no error if no value of the userId is passed.
-//4. if any of the field is additional throw error.
-// 5. update the field and return the response that user has been updated.
-// 6. use the try-catch as before.
-
-// we always need to sanitise the data coming from the api. never trust data coming from the user.
-
-// eg 2: the skills arrays must have the length not more than 10. this might be helpful when a hacker sends about the 1Millions skills in the array and DB may break.
-// so we can put the check for that as :
-//skills length validation
-// if (data?.skills.length > 10) {
-//   throw new Error("Skills can't be more than 10.");
- // }
